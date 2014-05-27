4c6db4a8c678d579c861b5bc5966da7dd8e08ff4
Chris Done <chrisdone@gmail.com>
2014-05-27 17:15:26 +0200

    Add rendering

--- src/Senza.hs
+++ src/Senza.hs
@@ -2,8 +2,17 @@
 
 module Senza
   (module Senza.Elements
-  ,module Senza.Types)
+  ,module Senza.Types
+  ,renderSenza)
   where
 
 import Senza.Elements
 import Senza.Types
+
+import Data.Text.Lazy (Text)
+import Text.Blaze.Html.Renderer.Text (renderHtml)
+
+-- | Will protect other codebases from when Text.Blaze.Html eventually
+-- gets renamed to Text.Blaze.Markup or something.
+renderSenza :: Senza -> Text
+renderSenza = renderHtml
